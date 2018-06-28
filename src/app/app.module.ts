abf72ab0ae6f1aace63491dc1ab536a298ca8df7
udara94 <37542994+udara94@users.noreply.github.com>
2018-06-28 22:39:29 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +28,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Gyroscope,
+    DeviceMotion
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope';
+import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,66 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public xOrient:any;
+  public yOrient:any;
+  public zOrient:any;
+  public timestamp:any
+  public accX:any;
+  public accY:any;
+  public accZ:any;
 
+  constructor(public navCtrl: NavController,
+    private gyroscope: Gyroscope,
+    private deviceMotion: DeviceMotion) {
+
+   
   }
 
+  gyrascope(){
+
+    let options: GyroscopeOptions = {
+      frequency: 1000
+   };
+   
+   this.gyroscope.getCurrent(options)
+     .then((orientation: GyroscopeOrientation) => {
+        console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+        this.xOrient=orientation.x;
+        this.yOrient=orientation.y;
+        this.zOrient=orientation.z;
+        this.timestamp=orientation.timestamp;
+
+      })
+     .catch()
+   
+   
+   this.gyroscope.watch()
+      .subscribe((orientation: GyroscopeOrientation) => {
+         console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+         this.xOrient=orientation.x;
+        this.yOrient=orientation.y;
+        this.zOrient=orientation.z;
+        this.timestamp=orientation.timestamp;
+      });
+  }
+
+  Accelerometer(){
+    this.deviceMotion.getCurrentAcceleration().then(
+      (acceleration: DeviceMotionAccelerationData) =>
+       console.log(acceleration),
+   
+    //  (error: any) => console.log(error)
+ 
+    );
+    
+    // Watch device acceleration
+    var subscription = this.deviceMotion.watchAcceleration().subscribe((acceleration: DeviceMotionAccelerationData) => {
+      console.log(acceleration);
+      this.accX=acceleration.x;
+      this.accY=acceleration.y;
+      this.accZ=acceleration.z;
+    });
+    
+  }
+ 
 }
